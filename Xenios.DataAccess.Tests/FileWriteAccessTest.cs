3a791e4749079c33460552ddf1e3d54839b8dedd
mcvangelder <vangelder@gmail.com>
2015-08-25 18:13:53 -0400

    Persist Information Failing Test

---
+++ Xenios.DataAccess.Tests/FileWriteAccessTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Xenios.DataAccess.Tests
+{
+    [TestClass]
+    public class FileWriteAccessTest
+    {
+        [TestMethod]
+        public void Should_persist_insurance_info()
+        {
+            var fileName = "insurance_information_storage.txt";
+            var infoService = new DataAccess.InsuranceInformationService(fileName);
+            var insuranceInformation = new DomainModels.InsuranceInformation
+            {
+                Id = Guid.NewGuid(),
+                CustomerFirstName = "John",
+                CustomerLastName = "Smith",
+                AddressLine1 = "123 Some Street",
+                City = "City",
+                State = "State",
+                PostalCode = "12345",
+                InsuranceType = 0,
+                CoverageBeginDateTime = DateTime.Now,
+                CreditCardType = 0,
+                CreditCardNumber = "1234-5678-9012-3456",
+                CountryOfBirth = "United States",
+                Price = (decimal)274.00,
+                TermLength = 6,
+                TermUnit = 0
+            };
+
+            infoService.Save(insuranceInformation);
+            var savedInformation = infoService.GetAll().FirstOrDefault(i => i.Id == insuranceInformation.Id);
+
+            Assert.IsNotNull(savedInformation);
+        }
+    }
+}
---
+++ Xenios.DataAccess/InsuranceInformationService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xenios.DomainModels;
+
+namespace Xenios.DataAccess
+{
+    public class InsuranceInformationService
+    {
+        private string _fileName;
+
+        public InsuranceInformationService(string fileName)
+        {
+            _fileName = fileName;
+        }
+
+        public void Save(InsuranceInformation insuranceInformation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<InsuranceInformation> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
