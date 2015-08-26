719ffe4111ef68975262eae523445eb4ef7c2f17
mcvangelder <vangelder@gmail.com>
2015-08-25 22:29:47 -0400

    Test added for verifying simultaneous saving to storage file is not permitted

--- Xenios.DataAccess.Tests/DataPersistenceTest.cs
+++ Xenios.DataAccess.Tests/DataPersistenceTest.cs
@@ -2,26 +2,17 @@
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace Xenios.DataAccess.Tests
 {
     [TestClass]
     public class DataPersistenceTest
     {
-        private const String fileName = @"c:\temp\insurance_information_storage.txt";
+        private const String fileName = @"\\monticello\home\development\insurance_information_storage.txt";
 
-        [TestInitialize]
-        [TestCleanup]
-        public void DeletePersistenceFile()
+        private static Domain.Models.InsuranceInformation CreateInsuranceInformation()
         {
-            var persistenceFile = new FileInfo(fileName);
-            persistenceFile.Delete();
-        }
-
-        [TestMethod]
-        public void Should_persist_insurance_info()
-        {
-            var infoService = new DataAccess.InsuranceInformationService(fileName);
             var insuranceInformation = new Domain.Models.InsuranceInformation
             {
                 Id = Guid.NewGuid(),
@@ -48,6 +39,22 @@ namespace Xenios.DataAccess.Tests
                 TermLength = 6,
                 TermUnit = 0
             };
+            return insuranceInformation;
+        }
+
+        [TestInitialize]
+        [TestCleanup]
+        public void DeletePersistenceFile()
+        {
+            var persistenceFile = new FileInfo(fileName);
+            persistenceFile.Delete();
+        }
+
+        [TestMethod]
+        public void Should_persist_insurance_info()
+        {
+            var infoService = new DataAccess.InsuranceInformationService(fileName);
+            var insuranceInformation = CreateInsuranceInformation();
 
             infoService.Save(insuranceInformation);
             var savedInformation = infoService.GetAll().FirstOrDefault(i => i.Id == insuranceInformation.Id);
@@ -71,6 +78,18 @@ namespace Xenios.DataAccess.Tests
             Assert.AreEqual(insuranceInformation.PaymentInformation.Id, savedInformation.PaymentInformation.Id);
             Assert.AreEqual(insuranceInformation.PaymentInformation.CreditCardNumber, savedInformation.PaymentInformation.CreditCardNumber);
             Assert.AreEqual(insuranceInformation.PaymentInformation.CreditCardType, savedInformation.PaymentInformation.CreditCardType);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IOException))]
+        public void Should_throw_exception_writing_to_file_opened_for_write_by_another_process()
+        {
+            using (var fs = new FileStream(fileName, FileMode.Create, FileAccess.Write))
+            {
+                var informationService = new InsuranceInformationService(fileName);
+                var insuranceInformation = CreateInsuranceInformation();
+                informationService.Save(insuranceInformation);
+            }
         }
     }
 }
