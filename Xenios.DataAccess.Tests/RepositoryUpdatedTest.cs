7609767f1ddbfa9fd0309f33907456bf9aac9a9e
mcvangelder <vangelder@gmail.com>
2015-08-26 18:27:54 -0400

    Added RepositoryUpdateNotificationService Test

---
+++ Xenios.DataAccess.Tests/RepositoryUpdatedTest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Threading;
+
+namespace Xenios.DataAccess.Tests
+{
+    [TestClass]
+    public class RepositoryUpdatedTest
+    {
+        private string _fileName = @"temp_repository.txt";
+        private InsuranceInformationRepository repository;
+
+        [TestInitialize]
+        public void CreateRepository()
+        {
+            repository = new InsuranceInformationRepository(_fileName);
+        }
+
+        [TestCleanup]
+        public void DeleteRepository()
+        {
+            File.Delete(_fileName);
+        }
+
+        [TestMethod]
+        public void Should_detect_repository_updates()
+        {
+            DataAccess.RepositoryUpdatedNotificationService notificationService = 
+                new RepositoryUpdatedNotificationService(repository);
+
+            var repositoryUpdatedCalled = new ManualResetEvent(false);
+
+            notificationService.NotifyRepositoryUpdated += (repo) =>
+                {
+                    repositoryUpdatedCalled.Set();
+                };
+
+            var insuranceInformation = new Domain.Models.InsuranceInformation
+            {
+                Id = Guid.NewGuid(),
+                Customer = new Domain.Models.CustomerInformation
+                {
+                    Id = Guid.NewGuid(),
+                    FirstName = "John",
+                    LastName = "Smith",
+                    AddressLine1 = "123 Some Street",
+                    City = "City",
+                    State = "State",
+                    PostalCode = "12345",
+                    Country = "United States",
+                },
+                PaymentInformation = new Domain.Models.PaymentInformation
+                {
+                    Id = Guid.NewGuid(),
+                    CreditCardType = Domain.Enums.CreditCardTypes.Amex,
+                    CreditCardNumber = "1234-5678-9012-3456",
+                    ExpirationDate = DateTime.Now,
+                    CreditCardVerificationNumber = "001"
+                },
+                InsuranceType = Domain.Enums.InsuranceTypes.Comprehensive,
+                CoverageBeginDateTime = DateTime.Now,
+                Price = (decimal)274.00,
+                TermLength = 6,
+                TermUnit = Domain.Enums.TermUnits.Months
+            };
+
+            repository.Save(insuranceInformation);
+            var isNotified = repositoryUpdatedCalled.WaitOne(TimeSpan.FromSeconds(1));
+            Assert.IsTrue(isNotified, "NotifyRepositoryUpdated was not called");
+
+        }
+    }
+}
---
+++ Xenios.DataAccess/RepositoryUpdatedNotificationService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xenios.DataAccess
+{
+    public delegate void RepositoryUpdated(InsuranceInformationRepository repository);
+
+    public class RepositoryUpdatedNotificationService
+    {
+        private InsuranceInformationRepository _repository;
+
+        public RepositoryUpdatedNotificationService(InsuranceInformationRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public event RepositoryUpdated NotifyRepositoryUpdated; 
+    }
+}
