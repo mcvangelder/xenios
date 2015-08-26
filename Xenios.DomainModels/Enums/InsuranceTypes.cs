1764032a4f5d12a0dac55fc8d249ae4bfeb96da8
mcvangelder <vangelder@gmail.com>
2015-08-25 22:56:44 -0400

    Added excluded file

---
+++ Xenios.DomainModels/Enums/InsuranceTypes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xenios.Domain.Enums
+{
+    public enum InsuranceTypes : byte
+    {
+        Comprehensive = 0,
+        Liability = 1,
+        Collision = 2,
+        Glass = 3,
+        Umbrella = 4
+    }
+}
