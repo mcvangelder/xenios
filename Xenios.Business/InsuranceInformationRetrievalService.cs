8c2dc01c546ac6e8ea67ca9824f1a492f90b47cc
mcvangelder <vangelder@gmail.com>
2015-08-26 23:09:42 -0400

    Added untracked files

---
+++ Xenios.Business/InsuranceInformationRetrievalService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xenios.Domain.Models;
+
+namespace Xenios.Business
+{
+    public class InsuranceInformationRetrievalService
+    {
+        private DataAccess.InsuranceInformationRepository _informationRepository;
+
+        public InsuranceInformationRetrievalService(String sourceFile)
+        {
+            _informationRepository = new DataAccess.InsuranceInformationRepository(sourceFile);
+        }
+
+        public List<InsuranceInformation> GetAllInsurancePolicies()
+        {
+            return _informationRepository.GetAll();
+        }
+    }
+}
