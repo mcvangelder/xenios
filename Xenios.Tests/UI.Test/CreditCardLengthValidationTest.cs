6d0b47637e27dfdbcc043ee725aec4e46cd92349
mcvangelder <vangelder@gmail.com>
2015-09-04 16:53:31 -0400

    Credit card validation rule for length 16

---
+++ Xenios.Tests/UI.Test/CreditCardLengthValidationTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xenios.UI.Infrastructure;
+
+namespace Xenios.UI.Test
+{
+    [TestClass]
+    public class CreditCardLengthValidationTest
+    {
+        [TestMethod]
+        public void Should_validate_value_of_length_16()
+        {
+            var value = "1234567890123456";
+            var validationRule = new CreditCardLengthValidationRule();
+
+            var result = validationRule.Validate(value, null);
+
+            Assert.IsTrue(result.IsValid);
+        }
+
+    }
+}
---
+++ Xenios.UI/Infrastructure/CreditCardLengthValidationRule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Xenios.UI.Infrastructure
+{
+    public class CreditCardLengthValidationRule : ValidationRule
+    {
+        private const int acceptedLength = 16;
+        public override ValidationResult Validate(object enteredValue, System.Globalization.CultureInfo cultureInfo)
+        {
+            var value = enteredValue as String;
+
+            return new ValidationResult(value.Length == acceptedLength, null);
+        }
+    }
+}
